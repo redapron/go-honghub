e4ab96693d8995b0bcd3ff12fff4289dcdddd384
bui <bungbui@gmail.com>
2018-12-01 22:50:36 +0700

    allow Method Header

--- go/routers.go
+++ go/routers.go
@@ -53,6 +53,8 @@ func Allow(inner http.Handler) http.Handler {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
