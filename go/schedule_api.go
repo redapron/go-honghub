46ccbb5dbe41287f132c1a996c2fb1320bb2d3f4
bui <bungbui@gmail.com>
2018-12-01 23:35:32 +0700

    query schedule by time

--- go/schedule_api.go
+++ go/schedule_api.go
@@ -127,3 +127,91 @@ func listSchedule() []Schedule {
 	fmt.Printf("scheds: %+v", scheds)
 	return scheds
 }
+
+// ===== Schedule - Filter
+func FilterSchedule(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var req struct {
+		Begin int64 `firestore:"begin,omitempty" json:"begin,omitempty"`
+		End   int64 `firestore:"end,omitempty" json:"end,omitempty"`
+	}
+
+	err := decoder.Decode(&req)
+	if err != nil {
+		fmt.Println("error decoding", err)
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	begin := time.Unix(req.Begin, 0)
+	end := time.Unix(req.End, 0)
+
+	fmt.Println("req.begin:", req.Begin)
+	fmt.Println("req.end:", req.End)
+	fmt.Println("begin:", begin)
+	fmt.Println("end:", end)
+
+	schedules := filterSchedule(begin, end)
+	l, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		fmt.Println("error load location")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Convert time to +7
+	for i, _ := range schedules {
+		fmt.Println("time:", schedules[i].Start)
+		schedules[i].Start = schedules[i].Start.In(l)
+		fmt.Println("time in bkk:", schedules[i].Start)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(schedules)
+}
+
+func filterSchedule(begin, end time.Time) []Schedule {
+	ctx := context.Background()
+
+	// [START fs_initialize]
+	// Sets your Google Cloud Platform project ID.
+	projectID := "honghub-224111"
+
+	// Get a Firestore client.
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Close client when done.
+	defer client.Close()
+	// [END fs_initialize]
+
+	// [START fs_get_all_users]
+	query := client.Collection("schedules").Where("time", ">=", begin).Where("time", "<=", end)
+	iter := query.Documents(ctx)
+
+	scheds := []Schedule{}
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+
+		var sd Schedule
+		doc.DataTo(&sd)
+		scheds = append(scheds, sd)
+	}
+	// [END fs_get_all_users]
+
+	fmt.Printf("query: %+v", query)
+	fmt.Printf("sched: %+v", scheds)
+	return scheds
+}
